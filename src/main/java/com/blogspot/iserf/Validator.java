ea6f857f2535ba70b7fc9ead9c0e1b1774e379aa
code-monster <ua-lex@yandex.ru>
2015-07-31 00:51:12 +0300

    fix validator date

--- src/main/java/com/blogspot/iserf/Validator.java
+++ src/main/java/com/blogspot/iserf/Validator.java
@@ -1,5 +1,10 @@
 package com.blogspot.iserf;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Validator {
 
 	public static String checkUser(User user) {
@@ -13,10 +18,21 @@ public class Validator {
 		} else if (user.getAddress().length() < 2) {
 			return "Field Address should have more than 1 letters";
 			
-		} else if (user.getDob().length() != 10) {
+		} else if (user.getDob().length() != 10) {				
+			
 			return "Field  date of birth is not valid";
 			
 		} 
+
+	     try {
+	            DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+	            Date result =  df.parse(user.getDob());
+	        } catch (ParseException pe) {
+	        	
+	        	return pe.getMessage();
+	        }
+		
+		
 
 		return "OK";
 
