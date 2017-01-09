9d8494f2192aaf55b637eff746c73262bf5dd1dc
code-monster <ua-lex@yandex.ru>
2017-01-10 00:41:41 +0200

    auto reformat code TransactionListController

--- src/main/java/com/blogspot/iserf/controllers/TransactionListController.java
+++ src/main/java/com/blogspot/iserf/controllers/TransactionListController.java
@@ -21,86 +21,63 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class TransactionListController {
 
-	private static final Logger logger = LoggerFactory.getLogger(TransactionListController.class);
+    private static final Logger logger = LoggerFactory.getLogger(TransactionListController.class);
 
-	@Autowired
-	private HttpServletRequest context;
+    @Autowired
+    private HttpServletRequest context;
 
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/transaction-list", method = RequestMethod.GET)
-	public ModelAndView editUser(Model model, @ModelAttribute("message") Message message)
-	{
+    @RequestMapping(value = "/transaction-list", method = RequestMethod.GET)
+    public ModelAndView editUser(Model model, @ModelAttribute("message") Message message) {
 
-		model.addAttribute("userList", UserDb.getUserList());
+        model.addAttribute("userList", UserDb.getUserList());
 
+        int userId;
+        if (context.getParameterMap().containsKey("showUserTransaction") == false) {
+            userId = 0;
+        } else {
 
-		int userId;
-		if (context.getParameterMap().containsKey("showUserTransaction") == false) {
-			userId = 0;
-		}else{
+            if (Validator.isInteger(context.getParameter("showUserTransaction")) == false) {
 
-			if (Validator.isInteger(context.getParameter("showUserTransaction"))==false) {
+                Message errorMessage = new Message("error", "Error in showUserTransaction param!");
+                model.addAttribute("message", errorMessage);
+                model.addAttribute("pageTitle", "Error page");
+                return new ModelAndView("error");
+            } else {
+                userId = new Integer(context.getParameter("showUserTransaction"));
+            }
 
-				Message errorMessage = new Message("error", "Error in showUserTransaction param!");
-				model.addAttribute("message", errorMessage);
-				model.addAttribute("pageTitle", "Error page");
-				return new ModelAndView("error");
-			}else{
-				userId = new Integer(context.getParameter("showUserTransaction"));
-			}
+        }
 
-		}
+        String startDateRangeSql = null;
+        String endDateRangeSql = null;
 
+        if (context.getParameterMap().containsKey("startDateRange") == true && context.getParameterMap().containsKey("endDateRange") == true) {
 
-		String startDateRangeSql = null;
-		String endDateRangeSql = null;
+            if (Validator.isDate(context.getParameter("startDateRange")) == false || Validator.isDate(context.getParameter("endDateRange")) == false) {
 
+                Message errorMessage = new Message("error", "Error in startDateRange or endDateRange param!");
+                model.addAttribute("message", errorMessage);
+                model.addAttribute("pageTitle", "Error page");
+                return new ModelAndView("error");
+            } else {
+                DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+                startDateRangeSql = context.getParameter("startDateRange");
+                endDateRangeSql = context.getParameter("endDateRange");
+            }
 
-		if (context.getParameterMap().containsKey("startDateRange") == true && context.getParameterMap().containsKey("endDateRange") == true) {
+        }
 
-			if (Validator.isDate(context.getParameter("startDateRange"))==false || Validator.isDate(context.getParameter("endDateRange"))==false) {
+        ArrayList<TransactionDisplay> transactionList = TransactionDb.getTransactionDisplayList(userId, startDateRangeSql, endDateRangeSql);
 
-				Message errorMessage = new Message("error", "Error in startDateRange or endDateRange param!");
-				model.addAttribute("message", errorMessage);
-				model.addAttribute("pageTitle", "Error page");
-				return new ModelAndView("error");
-			}else{
-
-				DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-
-
-					startDateRangeSql = context.getParameter("startDateRange");
-					//startDateRangeSql = new java.sql.Date(startDateRange.getTime());
-
-					endDateRangeSql = context.getParameter("endDateRange");
-					//endDateRangeSql = new java.sql.Date(endDateRange.getTime());
-
-
-			}
-
-		}
-
-
-		ArrayList<TransactionDisplay> transactionList = TransactionDb.getTransactionDisplayList(userId, startDateRangeSql, endDateRangeSql);
-
-		Breadcrumbs breadcrumbs = new Breadcrumbs(context);
-		breadcrumbs.add("Transactions list");
-		model.addAttribute("breadcrumbs", breadcrumbs);
-		model.addAttribute("pageTitle", "Transactions list");
-		model.addAttribute("message", message);
-		return new ModelAndView("transaction-list", "transactionList", transactionList);
-	}
-
-
-
-
+        Breadcrumbs breadcrumbs = new Breadcrumbs(context);
+        breadcrumbs.add("Transactions list");
+        model.addAttribute("breadcrumbs", breadcrumbs);
+        model.addAttribute("pageTitle", "Transactions list");
+        model.addAttribute("message", message);
+        return new ModelAndView("transaction-list", "transactionList", transactionList);
+    }
 
 }
